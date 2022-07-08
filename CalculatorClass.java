bc11c7d9b756385752cc7262f0824031c0b742a3
namratha-26 <namratha.ip@wissen.com>
2022-07-08 16:35:13 +0530

    adding sub()

--- CalculatorClass.java
+++ CalculatorClass.java
@@ -3,6 +3,10 @@ class Calculator {
 	{
 		return a+b;	
 	}
+	public int sub(int a, int b)
+	{
+		return a-b;
+	}
 }
 public class CalculatorClass
 {
@@ -10,5 +14,6 @@ public class CalculatorClass
 	{
 		Calculator c = new Calculator();
 		System.out.println("Result of addition: "+c.add(20,10));
+		System.out.println("Result of Subtraction: "+c.sub(20,10));
 	}
 }
