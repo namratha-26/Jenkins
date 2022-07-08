1da522e4bc3e5c8a76133e421d1a7772873ed032
namratha-26 <namratha.ip@wissen.com>
2022-07-08 16:26:04 +0530

    add new file: CalculatorClass.java

---
+++ CalculatorClass.java
@@ -0,0 +1,14 @@
+class Calculator {
+	public int add(int a, int b)
+	{
+		return a+b;	
+	}
+}
+public class CalculatorClass
+{
+	public static void main(String args[])
+	{
+		Calculator c = new calculator();
+		System.out.println("Result of addition: "+c.add(20,10));
+	}
+}
